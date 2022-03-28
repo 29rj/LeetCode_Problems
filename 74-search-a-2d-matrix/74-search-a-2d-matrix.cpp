9c705cecb2daadf5ba691cce53673ef66272c64f
Rishabh Raj <70429608+29rj@users.noreply.github.com>
2022-03-28 16:29:14 +0530

    Time: 6 ms (48.84%), Space: 9.6 MB (51.99%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
@@ -23,22 +23,43 @@ public:
     
     bool searchMatrix(vector<vector<int>>& mat, int target) {
         
-        int row = -1 , col = mat[0].size();
-        col--;
+//         int row = -1 , col = mat[0].size();
+//         col--;
         
-        for(int i=0;i<mat.size();i++){
+//         for(int i=0;i<mat.size();i++){
             
-            if(target < mat[i][col] ){
-                row = i;
-                break;
-            }
-            else if(target == mat[i][col])
+//             if(target < mat[i][col] ){
+//                 row = i;
+//                 break;
+//             }
+//             else if(target == mat[i][col])
+//                 return true;
+//         }
+        
+//         if(row == -1)
+//             return false;
+        
+//         return bs(mat,target,row);
+        
+        //applying bs on 2d directly 
+        
+        int n = mat.size(), m = mat[0].size();
+        
+        int st = 0 , end = m*n-1;
+        
+        while(st<=end){
+            
+            int mid = (st+end)/2;
+            
+            if(mat[mid/m][mid%m]  == target){
                 return true;
+            }
+            else if(mat[mid/m][mid%m] < target){
+                st = mid+1;
+            }
+            else
+                end = mid -1;
         }
-        
-        if(row == -1)
-            return false;
-        
-        return bs(mat,target,row);
+        return false;
     }
 };
