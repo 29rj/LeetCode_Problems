3c6b5dee7f893b5ce21513ab7f6295508aadc433
Rishabh Raj <70429608+29rj@users.noreply.github.com>
2022-04-10 11:06:29 +0530

    Time: 0 ms (100.00%), Space: 8.5 MB (28.60%) - LeetHub

--- 682-baseball-game/682-baseball-game.cpp
+++ 682-baseball-game/682-baseball-game.cpp
@@ -1,33 +1,21 @@
 class Solution {
 public:
     
-    void print (stack<int>stk){
-        
-        while(!stk.empty()){
-            cout << stk.top() << " ";
-            stk.pop();
-        }
-        
-        cout << "\n";
-    }
-    
-    
     int calPoints(vector<string>& ops) {
         stack<int>stk;
         
         for(auto&it:ops){
         
             if(it == "C"){
-                // int ch = stk.top();
+                
                 stk.pop();
-                print(stk);
+                
             }
             else if(it == "D"){
                 int n = stk.top();
                 n *= 2;
                 stk.push(n);
-                // cout << n << " ";
-                // print(stk);
+               
             }
             else if(it == "+"){
                 int n1 = stk.top();
@@ -38,15 +26,12 @@ public:
                 stk.push(n2);
                 stk.push(n1);
                 stk.push(n1+n2);
-                // print(stk);
                 
-                // cout << n1 << " " << n2 << " "<< n1+n2 << " ";
             }
             else{
                 int num = stoi(it);
                 stk.push(num);
-                // cout << num << " ";
-                // print(stk);
+                
             }
         }
         
