11d99638fd73b3acb01610ffeda5294af984b2f3
Rishabh Raj <70429608+29rj@users.noreply.github.com>
2022-05-13 18:34:38 +0530

    Time: 8 ms (11.19%), Space: 6.9 MB (13.09%) - LeetHub

---
+++ 377-combination-sum-iv/377-combination-sum-iv.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    
+    unordered_map<int,int>mp;
+    
+    int helper(vector<int>& nums, int sum, int target){
+        
+        if(sum > target){
+            return 0;
+        }
+        
+        if(sum == target)
+            return 1;
+        
+        if(mp.find(sum) != mp.end())
+            return mp[sum];
+        
+        int total = 0;
+        
+        for(int i=0;i<nums.size();i++){
+            total += helper(nums,sum+nums[i],target);
+        }
+        
+        mp[sum] = total;  
+        
+        return total;
+    }
+    
+    
+    int combinationSum4(vector<int>& nums, int target) {
+        
+        return helper(nums,0,target);   
+    }
+};
