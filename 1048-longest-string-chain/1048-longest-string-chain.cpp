59744601d29e67643cf3e4db7f4222bb72cd34e1
Rishabh Raj <70429608+29rj@users.noreply.github.com>
2022-06-16 08:35:05 +0530

    Time: 196 ms (30.46%), Space: 13.2 MB (74.38%) - LeetHub

--- 1048-longest-string-chain/1048-longest-string-chain.cpp
+++ 1048-longest-string-chain/1048-longest-string-chain.cpp
@@ -1,17 +1,46 @@
 class Solution {
-    public int longestStrChain(String[] words) {
-        Map<String, Integer> dp = new HashMap<>();
-        Arrays.sort(words, (a, b)->a.length() - b.length());
-        int res = 0;
-        for (String word : words) {
-            int best = 0;
-            for (int i = 0; i < word.length(); ++i) {
-                String prev = word.substring(0, i) + word.substring(i + 1);
-                best = Math.max(best, dp.getOrDefault(prev, 0) + 1);
+public:
+    int longestStrChain(vector<string>& words) {
+        
+        auto compare = [&](string &s1,string &s2){
+            if(s1.size() != s2.size()+1)
+                return false;
+            
+            int i=0,j=0;
+            
+            while(i<s1.size()){
+                
+                if(s1[i] == s2[j]){
+                    i++,j++;
+                }else{
+                    i++;
+                }
             }
-            dp.put(word, best);
-            res = Math.max(res, best);
+            
+            return s1.size() == i && s2.size() == j;
+        };
+        
+        
+        //LIS
+        sort(words.begin(),words.end(),[&](string &w1,string &w2){
+            return w1.length()<w2.length();
+        });
+            
+        vector<int>dp(words.size(),1);
+        
+        int mx = 1;
+        
+        for(int i=1;i<words.size();i++){
+            for(int j=0;j<i;j++){
+                if(compare(words[i],words[j]) && dp[i]<dp[j]+1){
+                    dp[i] = dp[j] + 1;
+                }
+            }
+            
+            mx = max(dp[i],mx);
         }
-        return res;
+        
+        
+        return mx;
     }
-}
+};
