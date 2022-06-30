f8b2ba9babd2fd8a39fddff1ae6a5c63c13eb326
Rishabh Raj <70429608+29rj@users.noreply.github.com>
2022-06-30 09:40:14 +0530

    Added solution - LeetHub

---
+++ Number of ways - GFG/number-of-ways.java
@@ -0,0 +1,39 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(in.readLine());
+        while(t-- > 0){
+            int N = Integer.parseInt(in.readLine().trim());
+            
+            Solution ob = new Solution();
+            System.out.println(ob.arrangeTiles(N));
+        }
+    }
+}// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution{
+   static long arrangeTiles(int N){
+       // code here
+       long[] dp =new long[81];
+       // dp[0]=1;
+       // dp[1]=1;
+       // dp[2]=1;
+       // dp[3]=1;
+       Arrays.fill(dp,1);
+       for(long i=4;i<=N;i++)
+       {
+           dp[(int)i]=dp[(int)i-4]+dp[(int)i-1];
+       }
+       return dp[N];
+   }
+}
