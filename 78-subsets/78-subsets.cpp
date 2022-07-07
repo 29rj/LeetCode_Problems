98289507e9d90bbb5f24dfabef54c58144bf41a5
Rishabh Raj <70429608+29rj@users.noreply.github.com>
2022-07-07 20:54:54 +0530

    Time: 9 ms (18.90%), Space: 7.4 MB (51.11%) - LeetHub

--- 78-subsets/78-subsets.cpp
+++ 78-subsets/78-subsets.cpp
@@ -1,26 +1,21 @@
 class Solution {
 public:
-   
     vector<vector<int>> subsets(vector<int>& nums) {
         
-        function<void(set<vector<int>>&,int,vector<int>&,vector<int>)> helper = [&](set<vector<int>>&ans,int curr,vector<int>&nums,vector<int>vec){
+        function<void(vector<int>&,int,vector<int>,vector<vector<int>>&)> helper = [&](vector<int>&nums,int curr,vector<int>vec,vector<vector<int>>&ans){
             
-            if(curr == nums.size()){
-                ans.insert(vec);
-                return;
+            ans.push_back(vec);
+            
+            for(int i=curr;i<nums.size();i++){
+                vec.push_back(nums[i]);
+                helper(nums,i+1,vec,ans);
+                vec.pop_back();
             }
-            
-            vec.push_back(nums[curr]);
-            helper(ans,curr+1,nums,vec);
-            vec.pop_back();
-            helper(ans,curr+1,nums,vec);
         };
         
-        set<vector<int>>ans;
         vector<int>vec;
-        helper(ans,0,nums,vec);
-        
-        vector<vector<int>>fans(ans.begin(),ans.end());
-        return fans;
+        vector<vector<int>>ans;
+        helper(nums,0,vec,ans);
+        return ans;
     }
 };
