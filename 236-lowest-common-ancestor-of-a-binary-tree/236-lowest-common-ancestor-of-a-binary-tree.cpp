0fb8ebbf117a257a56b08386dc2eb879f5f2566a
Rishabh Raj <70429608+29rj@users.noreply.github.com>
2022-07-19 19:22:18 +0530

    Time: 30 ms (32.18%), Space: 14.1 MB (85.91%) - LeetHub

--- 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.cpp
+++ 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.cpp
@@ -9,58 +9,21 @@
  */
 class Solution {
 public:
-    TreeNode* path(TreeNode*root,TreeNode*dest,vector<TreeNode*>&vec){
-        if(root == NULL)
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if(!root)
             return NULL;
         
-        if(root == dest){
-            vec.push_back(root);
+        if(root == p || root == q || root == NULL)
             return root;
-        }
         
-        TreeNode* left = path(root->left,dest,vec);
-        TreeNode* right = path(root->right,dest,vec);
+        TreeNode* left = lowestCommonAncestor(root->left,p,q);
+        TreeNode* right = lowestCommonAncestor(root->right,p,q);
         
-        if(left || right){
-            vec.push_back(root);
+        if(left && right){
             return root;
+        }else{
+            return left ? left : right;
         }
         
-        return NULL;
-    }
-    
-    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        vector<TreeNode*>vec1,vec2;
-        
-        path(root,p,vec1);
-        path(root,q,vec2);
-        
-        reverse(vec1.begin(),vec1.end());
-        
-        reverse(vec2.begin(),vec2.end());
-        
-//         for(auto&it:vec1){
-//             cout << it->val << " ";
-//         }
-        
-//         cout << "\n";
-        
-//         for(auto&it:vec2){
-//             cout << it->val << " ";
-//         }
-        
-        for(int i=1;i<min(vec1.size(),vec2.size());i++){
-            
-            if(vec1[i] != vec2[i]){
-                return vec1[i-1];
-            }
-        }
-        
-        if(vec1.size() < vec2.size()){
-            return vec1.back();
-        }else
-            return vec2.back();
-        
-        return root;
     }
 };
